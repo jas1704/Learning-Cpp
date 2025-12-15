a05d9d66d68e208ee95fef7fc14b841038fdab58
jas1704 <152225394+jas1704@users.noreply.github.com>
2025-12-16 00:30:05 +0530

    Add files via upload

---
+++ Day 1/comments.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // single line comment
+
+    /* this
+    is a 
+    Multi Line Comment*/
+
+    // ctrl + / vs code shorcut for comments
+
+    return 0;
+}
